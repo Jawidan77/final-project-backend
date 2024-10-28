0acd6c8fbd9d8b758f0a6697b17f1f7499f46577
Jawidan77 <jawidan.reza77@gmail.com>
2024-10-28 10:58:07 -0400

    new update

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import dotenv from 'dotenv'
 
+dotenv.config()
 
 //app config
 const app = express()
